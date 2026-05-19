cf4161c2337ad23d14c480268b21865af7bd8201
Ahajsan <bartek.prus2004@gmail.com>
2026-05-19 21:21:25 +0000

    Deploying to gh-pages from @ wi-game-techpriests/wi_game@f35a43adccebcbe3e6076600e49b54724a46f1f6 🚀

--- WebGL/Build/WebGL.loader.js
+++ WebGL/Build/WebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 56836667,
+    wasmFileSize: 56836510,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
