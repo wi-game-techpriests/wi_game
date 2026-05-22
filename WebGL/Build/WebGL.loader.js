48bffecd68ceb879469844ea13d78b6c672924eb
Ahajsan <Ahajsan@users.noreply.github.com>
2026-05-22 17:13:21 +0000

    Deploying to gh-pages from @ wi-game-techpriests/wi_game@3da8e100b54b1c458f58e0a402070ce6ded11479 🚀

--- WebGL/Build/WebGL.loader.js
+++ WebGL/Build/WebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 69479300,
+    wasmFileSize: 69497645,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
