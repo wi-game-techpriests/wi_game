f2ff3d00d209f43d185c18bce29e57d46bd2db50
TheVTV <TheVTV@users.noreply.github.com>
2026-05-25 14:39:17 +0000

    Deploying to gh-pages from @ wi-game-techpriests/wi_game@fe00eee910817b330271b5e8df6a680784bfd20b 🚀

--- WebGL/Build/WebGL.loader.js
+++ WebGL/Build/WebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 69498807,
+    wasmFileSize: 69481345,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
