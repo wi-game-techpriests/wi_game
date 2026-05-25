7046e198e1ed64e75bbe74e7deb907988feeb3d5
Bartosz Wójcik <bartoszw004@gmail.com>
2026-05-25 16:22:58 +0200

    Restart
    - user can play each minigame max 3 times, after that "try again" button disappears
    - score is stored in GameManager now
    - after merging do what i said on dc

--- wi_game/Assets/Scripts/MainGameController.cs
+++ wi_game/Assets/Scripts/MainGameController.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainGameController : MonoBehaviour
 {
     public GameObject info;
     public GameObject game;
     public GameObject summary;
+
+    public Button retryButton;
+
+    public TMP_Text scoreText; 
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,16 +27,40 @@ public class MainGameController : MonoBehaviour
 
     public void StartGame()
     {
-        info.SetActive(false);
-        game.SetActive(true);
-        summary.SetActive(false);
+        int tries = GameManager.Instance.GetCurrentTries();
+        if (tries > 0)
+        {
+            GameManager.Instance.SetCurrentTries();
+            info.SetActive(false);
+            game.SetActive(true);
+            summary.SetActive(false);
+        }
     }
 
+
+    // TODO: visualize MAX score
     public void EndGame()
     {
+        int score = GameManager.Instance.GetCurrentScore();
+        int tries = GameManager.Instance.GetCurrentTries();
+
+        scoreText.text = score.ToString();
+        if (GameManager.Instance.GetSceneResult() < score)
+        {
+            GameManager.Instance.SetSceneResult(score);
+        }
+
         info.SetActive(false);
         game.SetActive(false);
         summary.SetActive(true);
+        if (tries > 0)
+        {
+            retryButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            retryButton.gameObject.SetActive(false);
+        }
     }
 
     public void NextMinigame()
