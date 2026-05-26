4f4a733bd2dc245a0e024af270ad96ca77c6b6d9
bartex555 <bartex555@users.noreply.github.com>
2026-05-26 16:53:20 +0000

    Deploying to gh-pages from @ wi-game-techpriests/wi_game@fe811633de6696e0b5a5a0b2d33b055daaefc601 🚀

--- WebGL/Build/WebGL.loader.js
+++ WebGL/Build/WebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 69540096,
+    wasmFileSize: 69538951,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
