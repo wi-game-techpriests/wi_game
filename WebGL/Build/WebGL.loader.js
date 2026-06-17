25b51f4f2b62f66839512bbf3a0677234a2f6e59
Devedir <Devedir@users.noreply.github.com>
2026-06-17 11:54:42 +0000

    Deploying to gh-pages from @ wi-game-techpriests/wi_game@b22336a36b35da6019e4034480053c94dc3841fa 🚀

--- WebGL/Build/WebGL.loader.js
+++ WebGL/Build/WebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 69586232,
+    wasmFileSize: 69588514,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
